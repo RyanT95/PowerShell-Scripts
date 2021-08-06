b6114075f26eec8201d011bf38aaba696ff2d1a9
Ryan Tinman <Ryan_Tinman95@hotmail.co.uk>
2021-08-06 12:48:21 +0100

    Update EXCH-Remove-User-DL.ps1
    Wrong file committed, replaced with correct code

--- Exchange/RemoveUserDistList/EXCH-Remove-User-DL.ps1
+++ Exchange/RemoveUserDistList/EXCH-Remove-User-DL.ps1
@@ -1,6 +1,6 @@
-﻿#************************************************
-# Batch add users to distribution list from CSV *
-#************************************************
+#******************************************
+# Remove user from all Distribution Lists *
+#******************************************
 
 # Check script is running as admin
 #=============================================================
@@ -34,154 +34,134 @@ Write-Host "Running as admin...`n" -ForegroundColor Green
 #=============================================================
 
 #**************************
-# Add users from CSV to DL
+# Find inputted user in AD
 #**************************
-Function Rem-Users
+Function Get-User-Details
 {
-    # USER INPUT: distribution list name
-    $distGroupName = Read-Host -Prompt 'Input full email address of distribution list'
-    
-    Write-Host "`n-----------------------------------------------------------------------------`n" -ForegroundColor DarkGray
-    
-    # Error checking for valid distribution list
-    try
-    {
-        Get-DistributionGroup -Identity $distGroupName -ErrorAction Stop | format-table
-    }
-    catch
-    {
-        Write-Host "$distGroupName is not a valid distribution list! Check spelling." -ForegroundColor DarkRed
-        exit
-    }
-    
-    Write-Host "-----------------------------------------------------------------------------`n" -ForegroundColor DarkGray
-    
-    Write-Host "Removing users from Distribution List...`n=======================================`n" -ForegroundColor Cyan
-    
-    # Delete contents of file to ensure only output shown is from the current instance
-    if(Test-Path $PSScriptRoot\Incorrect-Names.txt -PathType Leaf)
-    {
-        Clear-Content $PSScriptRoot\Incorrect-Names.txt
-    }
-    
-    # Add users from CSV file to specified DL
-    #=============================================================
-    foreach ($user in $users)
-    {
-        $email = $user.'Email'
-        $group = $distGroupName
-    
-        # Check user email is valid
-        #===========================
-        if(-not ( Get-ADUser -Filter "EmailAddress -eq '$email'" -ErrorAction SilentlyContinue)) # Email is NOT user
-        {
-            if(-not (Get-DistributionGroup -Identity $email -ErrorAction SilentlyContinue)) # Email is NOT DL
-            {
-                if(-not (Get-Mailbox -RecipientTypeDetails SharedMailbox -Identity $email -ErrorAction SilentlyContinue)) # Email is NOT mailbox
-                {
-					if(-not (Get-MailContact -Identity $email -ErrorAction SilentlyContinue)) # Email is NOT contact
-                    {
-						Write-Host " ERROR   : $email does not exist" -ForegroundColor DarkRed
-		
-						"$email" | Out-File -FilePath $PSScriptRoot\Incorrect-Names.txt -Append
-		
-						continue
-					}
-					else # Email IS contact
-					{
-						$selectedUser = Get-MailContact -Identity $email
-					}
-                }
-                else # Email IS mailbox
-                {
-                    $selectedUser = Get-Mailbox -RecipientTypeDetails SharedMailbox -Identity $email
-                }
-            }
-            else # Email IS DL
-            {
-                $selectedUser = Get-DistributionGroup -Identity $email
-            }   
-        }
-        else # Email IS user
-        {
-            $selectedUser = Get-ADUser -Filter "EmailAddress -eq '$email'"
-        }
-		
-    
-        # Check if user is a member of the DL
-        #=====================================
-        $groupMembers = Get-DistributionGroupMember -Identity $distGroupName
-        
-        if(($selectedUser) -ilike (Get-DistributionGroup -Identity $distGroupName))
-        {
-            Write-Host " WARN    : Cannot add $email to itself!" -ForegroundColor Red
-            continue         
-        }
+    # USER INPUT: user email
+    Write-Host "`nInput full email address of user : " -ForegroundColor Green -NoNewline
+    $email = Read-Host
 
-        if(-not ("$groupMembers".Contains($selectedUser.Name)))
+    Write-Host "`n-----------------------------------------------------------------------------`n" -ForegroundColor DarkGray -NoNewline
+    Write-Host "USER DETAILS`n" -ForegroundColor DarkGray -NoNewline
+    Write-Host "-----------------------------------------------------------------------------`n" -ForegroundColor DarkGray -NoNewline
+
+    #Can be shared inbox, but can't be DL#
+
+    # Check user email is valid
+    #===========================
+    if(-not (Get-ADUser -Filter "EmailAddress -eq '$email'" -ErrorAction SilentlyContinue)) # Email is NOT user
+    {
+        if(-not (Get-DistributionGroup -Identity $email -ErrorAction SilentlyContinue)) # Email is NOT DL
         {
-            Write-Host " WARN    : $email is not a member of the DL" -ForegroundColor Yellow
+            if(-not (Get-Mailbox -RecipientTypeDetails SharedMailbox -Identity $email -ErrorAction SilentlyContinue)) # Email is NOT mailbox
+            {
+                Write-Host " ERROR: $email does not exist" -ForegroundColor DarkRed
+                
+                Disconnect-ExchangeOnline -Confirm:$false
+                continue
+            }
+            else # Email IS mailbox
+            {
+                $isMailbox = $true
+                $selectedUser = Get-Mailbox -RecipientTypeDetails SharedMailbox -Identity $email
+            }
+        }
+        else # Email IS DL
+        {
+            Write-Host " ERROR: email cannot be Distribution List" -ForegroundColor DarkRed
+
+            Disconnect-ExchangeOnline -Confirm:$false
             continue
-        }
-    
-    
-        # Remove user from DL
-        #====================
-        try
-        {
-            Remove-DistributionGroupMember -Identity "$group" -member $email -BypassSecurityGroupManagerCheck -ErrorAction Stop -Confirm:$false
-        }
-        catch [System.Management.Automation.RemoteException]
-        {
-            Write-Host " WARN    : Unexpected Exception" -ForegroundColor DarkRed -NoNewLine
-			Write-Host " when removing: $email" -ForegroundColor Gray
-			
-            continue
-        }
-    
-        Write-Host " REMOVED : $email from $group" -ForegroundColor Green
+        }   
     }
-    #=============================================================
+    else # Email IS user
+    {
+        $isMailbox = $false
+        $selectedUser = Get-ADUser -Filter "EmailAddress -eq '$email'"
+    }
+
+    if ($isMailbox -eq $false)
+    {
+        (Get-ADUser -Filter "EmailAddress -eq '$email'" -Properties DisplayName, Title, EmailAddress | Format-List DisplayName, Title, EmailAddress| Out-String).trim()
+    }
+
+    if ($isMailbox -eq $true)
+    {
+        (Get-Mailbox -RecipientTypeDetails SharedMailbox -Identity $email | Format-List DisplayName, RecipientTypeDetails, PrimarySmtpAddress | Out-String).trim()
+    }
     
+    #===========================
+
+    Remove-User-DL($email)
+}
+
+#**************************
+# Remove user from all DLs
+#**************************
+Function Remove-User-DL($Email)
+{
+    Write-Host "`n-----------------------------------------------------------------------------`n" -ForegroundColor DarkGray -NoNewline
+    Write-Host "USER DISTRIBUTION GROUP MEMBERSHIPS`n" -ForegroundColor DarkGray -NoNewline
+    Write-Host "-----------------------------------------------------------------------------`n" -ForegroundColor DarkGray -NoNewline
+
+    $dn = Get-User $Email | select -ExpandProperty DistinguishedName
+    $groups = Get-Group -Filter "Members -eq '$dn'" -RecipientTypeDetails MailUniversalDistributionGroup | Select-Object -ExpandProperty DisplayName #| Out-String
+
+    foreach ($group in $groups)
+    {
+        Write-Host $group
+    }
+
+    # Confirm removal of DLs
+    #===========================
+    do
+    {
+        Write-Host "`nConfirm Removal of Groups? (y/n) : " -ForegroundColor Green -NoNewline
+
+        $confirm = Read-Host
+
+        switch ($confirm)
+	    {
+	    	y {Write-Host "Proceeding.`n"   -ForegroundColor Gray;  $valid = $true;  continue}
+            Y {Write-Host "Proceeding.`n"   -ForegroundColor Gray;  $valid = $true;  continue}
+	    	
+            n {Write-Host "Cancelling...`n" -ForegroundColor Gray;  $valid = $true;  return}
+            N {Write-Host "Cancelling...`n" -ForegroundColor Gray;  $valid = $true;  return}
+            
+	    	default {Write-Host "Invalid Selection! Try again.`n" -ForegroundColor DarkRed; $valid = $false;}
+	    }
+    }
+    until ($valid -eq $true)
+    #===========================
+
+    Write-Host "`nRemoving user from Distribution Lists...`n=======================================`n" -ForegroundColor Cyan
+
+    # Remove user from all Distribution Groups
+    foreach ($group in $groups)
+    {
+        #Remove-DistributionGroupMember -Identity "$group" -Member "$Email" -Confirm:$false
+
+        Write-Host "Removed : $Email from $group"
+    }
+
     Write-Host "`n=======================================`n" -ForegroundColor Cyan
+
     Write-Host "Done!`n" -ForegroundColor Green
 }
 
-#************************
-# Show results of script
-#************************
-Function Show-Results
-{
-    Write-Host "The following users were not found:" -ForegroundColor Red
-    Write-Host "=======================================`n" -ForegroundColor Red
-    
-    Get-Content $PSScriptRoot\Incorrect-Names.txt
-    
-    Write-Host "`n=======================================`n" -ForegroundColor Red
-    
-    Write-Host "------------`nOutput saved to:`n$PSScriptRoot\Incorrect-Names.txt`n------------`n`n" -ForegroundColor Cyan
-}
-
-
 #*************
-# Entry point
+# Entry Point
 #*************
-
-# USER INPUT
-$domain = Read-Host -Prompt 'Input company domain name (i.e. @domain.com)'
 
 # Get user who ran script
-$admCred = $env:UserName + $domain
+$admCred = $env:UserName + "@DOMAIN.com"
 
 # Connect to MS Exchange
 Import-Module ExchangeOnlineManagement -Verbose:$false
 Connect-ExchangeOnline -UserPrincipalName $admCred -ShowBanner:$false -ShowProgress:$true -Verbose:$false
 
-# CSV
-$users = Import-Csv -Delimiter "," -Path "$PSScriptRoot\Contacts.csv"
-
-Rem-Users
-Show-Results
+Get-User-Details
 
 Write-Host "Disconnecting from MS Exchange..." -ForegroundColor Gray
 Disconnect-ExchangeOnline -Confirm:$false
